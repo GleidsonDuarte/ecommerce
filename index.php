91cb8a95f70efd2fdcc6fede9c4df8e80dbeaa4f
Gleidson Duarte <gleidson.duarte@hotmail.com.br>
2018-07-23 18:15:53 -0300

    Criada a classe Page

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
-<?php 
+<?php
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\DB\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
 
-	$results = $sql->select("SELECT * FROM tb_users");
+	$page = new Page();
 
-	echo json_encode($results);
+	$page->setTpl("index");
 
 });
 
